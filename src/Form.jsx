ec7525e9b7e7fd17ab0ab765eb74003a3875ef49
Nasir <nasirbhatti.nk@gmail.com>
2024-11-13 14:56:45 +0500

    Add html form with some styles

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,7 +4,7 @@ const Form = () => {
   return (
     <div className="w-2/3">
       <div className="h-1/3 p-10 bg-gray-200">
-        <h3 className="w-4/5 mt-16 text-xl font-semibold text-gray-700">
+        <h3 className="w-4/5 mt-16 text-xl font-semibold text-gray-600">
           This is not a real online service! You know you need something like
           this in your life to help you realize your deepest dreams.
           <br />
@@ -14,15 +14,92 @@ const Form = () => {
           <h3>You know you want to.</h3>
         </h3>
       </div>
-      <div className="bg-white h-1/3 p-10"></div>
-      <div className="bg-gray-200 h-1/3 p-10">
+      <div className="bg-white h-1/3 p-10">
+        <h3 className="text-xl font-semibold text-gray-600 mb-4">
+          Let's do this!
+        </h3>
+        <form className="w-4/6 flex justify-between">
+          <div>
+            {" "}
+            <label className="flex flex-col text-gray-500 text-xs font-semibold">
+              FIRST NAME
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                className="w-56 rounded border-2 py-1 focus:outline-none focus:border-t-indigo-600  focus:ring-1 focus:ring-indigo-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200"
+                required
+                aria-required="true"
+              />
+            </label>
+            <label className="flex flex-col text-gray-500 font-semibold text-xs my-4">
+              EMAIL
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-56 rounded border-2 py-1 focus:outline-none focus:border-t-indigo-600  focus:ring-1 focus:ring-indigo-500"
+                required
+                aria-required="true"
+              />
+            </label>
+            <label className="flex flex-col text-gray-500 font-semibold text-xs">
+              PASSWORD
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-56 rounded border-2 py-1 focus:outline-none focus:border-t-indigo-600  focus:ring-1 focus:ring-indigo-500 "
+                required
+                aria-required="true"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="flex flex-col text-gray-500 font-semibold text-xs">
+              LAST NAME
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                className="w-56 rounded border-2 py-1 focus:outline-none focus:border-t-indigo-600  focus:ring-1 focus:ring-indigo-500 "
+                required
+                aria-required="true"
+              />
+            </label>
+            <label className="flex flex-col text-gray-500 font-semibold text-xs my-4">
+              PHONE NUMBER
+              <input
+                type="tel"
+                id="phoneNumber"
+                name="phoneNumber"
+                className="w-56 rounded border-2 py-1 focus:outline-none focus:border-t-indigo-600  focus:ring-1 focus:ring-indigo-500 "
+                required
+                aria-required="true"
+              />
+            </label>
+            <label className="flex flex-col text-gray-500 font-semibold text-xs">
+              CONFIRM PASSWORD
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                className="w-56 rounded border-2 py-1 focus:outline-none focus:border-t-indigo-600  focus:ring-1 focus:ring-indigo-500"
+                required
+                aria-required="true"
+              />
+            </label>
+          </div>
+        </form>
+      </div>
+      <div className="bg-gray-300 h-1/3 p-10">
         <button
           type="submit"
-          className="py-2 px-8 bg-lime-900 text-white rounded"
+          className="py-3 px-12 bg-lime-900 text-white rounded"
         >
           Create Account
         </button>
-        <p className="mt-6 font-medium text-gray-700">
+        <p className="mt-6 font-medium text-gray-600">
           Already have an account?{" "}
           <button type="button" className="text-lime-900 font-semibold">
             Log in
--- src/SideImage.jsx
+++ src/SideImage.jsx
@@ -8,7 +8,7 @@ const logo = {
 const SideImage = () => {
   return (
     <div
-      className="w-1/3 bg-red-500 h-full relative"
+      className="w-1/3 relative"
       style={{
         backgroundImage: `url("https://images.unsplash.com/photo-1585202900225-6d3ac20a6962?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
         backgroundPosition: "center",
@@ -19,11 +19,11 @@ const SideImage = () => {
         <img
           src={logo.src}
           alt={logo.alt}
-          className="w-16 ml-12"
+          className="w-20 ml-10"
         />
-        <p className="text-7xl text-white mt-3" style={{fontFamily: "Skranji"}}>ODIN</p>
+        <p className="text-7xl text-white mt-6" style={{fontFamily: "Skranji"}}>ODIN</p>
       </div>
-      <cite className="absolute bottom-4 left-28 text-xs text-white">
+      <cite className="absolute bottom-4 left-28 text-xs text-gray-300">
         Photo by{" "}
         <a
           href="https://unsplash.com/photos/green-leaf-plant-in-close-up-photography-25xggax4bSA"
